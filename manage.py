89685fb7d1002b10ebc4e398ca0991ad195157d0
tiwari4321 <97392515+tiwari4321@users.noreply.github.com>
2024-12-15 23:15:27 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -19,4 +19,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    import os
+    from django.core.management import execute_from_command_line
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    
+    port = os.getenv('PORT', '8000')
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
