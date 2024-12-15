d02717156719ea581ccd4f33ca1725283cab5532
tiwari4321 <97392515+tiwari4321@users.noreply.github.com>
2024-12-15 23:39:59 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MESSAGE_TAGS ={
